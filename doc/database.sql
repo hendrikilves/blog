803b1395c67e14c12751f0615c39d2ba52e3b0ec
hendrikilves <hendrik.ilves@khk.ee>
2018-05-18 11:49:00 +0300

    Update database structure

--- doc/database.sql
+++ doc/database.sql
@@ -1,84 +1,231 @@
--- MySQL dump 10.13  Distrib 5.6.25, for osx10.6 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: halo
--- ------------------------------------------------------
--- Server version	5.6.25
+-- Host: localhost:3306
+-- Generation Time: May 18, 2018 at 11:48 AM
+-- Server version: 5.6.39
+-- PHP Version: 5.6.30
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `ilveshen_halo`
+--
+CREATE DATABASE IF NOT EXISTS `ilveshen_halo` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `ilveshen_halo`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `post_id` int(10) UNSIGNED NOT NULL,
+  `post_subject` varchar(255) NOT NULL,
+  `post_text` text NOT NULL,
+  `post_created,` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created,`, `user_id`) VALUES
+(1, 'Test pealkiri', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2018-05-18 06:15:52', 1),
+(2, 'Test pealkiri 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent blandit mi congue, tincidunt augue id, hendrerit quam. Morbi malesuada congue scelerisque. Suspendisse quis lorem fringilla erat cursus commodo sed in augue. Donec convallis auctor orci, ut imperdiet nisl vulputate id. Mauris dui enim, laoreet ut eleifend eu, fermentum vitae lacus. Morbi sagittis efficitur dui. Ut id risus at velit sollicitudin dignissim. ', '2018-05-18 06:37:43', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_tags`
+--
+
+DROP TABLE IF EXISTS `post_tags`;
+CREATE TABLE `post_tags` (
+  `post_id` int(11) UNSIGNED NOT NULL,
+  `tag_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `post_tags`
+--
+
+INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag` (
+  `tag_id` int(10) UNSIGNED NOT NULL,
+  `tag_name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tag`
+--
+
+INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
+(1, 'test1'),
+(2, 'test2');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `translations`
 --
 
 DROP TABLE IF EXISTS `translations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `translations` (
-  `translation_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `translation_id` int(10) UNSIGNED NOT NULL,
   `phrase` varchar(191) NOT NULL,
   `language` char(3) NOT NULL,
   `translation` varchar(191) DEFAULT NULL,
   `controller` varchar(15) NOT NULL,
-  `action` varchar(20) NOT NULL,
-  PRIMARY KEY (`translation_id`),
-  UNIQUE KEY `language_phrase_controller_action_index` (`language`,`phrase`,`controller`,`action`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `action` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `translations`
 --
 
-LOCK TABLES `translations` WRITE;
-/*!40000 ALTER TABLE `translations` DISABLE KEYS */;
-INSERT INTO `translations` VALUES (1,'Action','en','{untranslated}','welcome','index'),(2,'Server returned response in an unexpected format','en','{untranslated}','global','global'),(3,'Forbidden','en','{untranslated}','global','global'),(4,'Server returned an error','en','{untranslated}','global','global'),(5,'Action','en','{untranslated}','halo','index'),(6,'Logout','en','{untranslated}','global','global'),(7,'Settings','en','{untranslated}','global','global'),(8,'Logged in as','en','{untranslated}','global','global'),(9,'Log out','en','{untranslated}','global','global'),(10,'Action','et','{untranslated}','welcome','index'),(11,'Log out','et','{untranslated}','global','global'),(12,'Server returned response in an unexpected format','et','{untranslated}','global','global'),(13,'Forbidden','et','{untranslated}','global','global'),(14,'Server returned an error','et','{untranslated}','global','global'),(15,'Please sign in','et','{untranslated}','global','global'),(16,'Email','et','{untranslated}','global','global'),(17,'Password','et','{untranslated}','global','global'),(18,'Sign in','et','{untranslated}','global','global');
-/*!40000 ALTER TABLE `translations` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `translations` (`translation_id`, `phrase`, `language`, `translation`, `controller`, `action`) VALUES
+(1, 'Action', 'en', '{untranslated}', 'welcome', 'index'),
+(2, 'Server returned response in an unexpected format', 'en', '{untranslated}', 'global', 'global'),
+(3, 'Forbidden', 'en', '{untranslated}', 'global', 'global'),
+(4, 'Server returned an error', 'en', '{untranslated}', 'global', 'global'),
+(5, 'Action', 'en', '{untranslated}', 'halo', 'index'),
+(6, 'Logout', 'en', '{untranslated}', 'global', 'global'),
+(7, 'Settings', 'en', '{untranslated}', 'global', 'global'),
+(8, 'Logged in as', 'en', '{untranslated}', 'global', 'global'),
+(9, 'Log out', 'en', '{untranslated}', 'global', 'global'),
+(10, 'Action', 'et', '{untranslated}', 'welcome', 'index'),
+(11, 'Log out', 'et', '{untranslated}', 'global', 'global'),
+(12, 'Server returned response in an unexpected format', 'et', '{untranslated}', 'global', 'global'),
+(13, 'Forbidden', 'et', '{untranslated}', 'global', 'global'),
+(14, 'Server returned an error', 'et', '{untranslated}', 'global', 'global'),
+(15, 'Please sign in', 'et', '{untranslated}', 'global', 'global'),
+(16, 'Email', 'et', '{untranslated}', 'global', 'global'),
+(17, 'Password', 'et', '{untranslated}', 'global', 'global'),
+(18, 'Sign in', 'et', '{untranslated}', 'global', 'global'),
+(19, 'Please sign in', 'en', '{untranslated}', 'global', 'global'),
+(20, 'Email', 'en', '{untranslated}', 'global', 'global'),
+(21, 'Password', 'en', '{untranslated}', 'global', 'global'),
+(22, 'Sign in', 'en', '{untranslated}', 'global', 'global'),
+(23, 'Action', 'en', '{untranslated}', 'global', 'global'),
+(24, 'Oops...', 'en', '{untranslated}', 'global', 'global'),
+(25, 'Close', 'en', '{untranslated}', 'global', 'global'),
+(26, 'Server returned an error. Please try again later ', 'en', '{untranslated}', 'global', 'global');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) UNSIGNED NOT NULL,
   `is_admin` tinyint(4) NOT NULL DEFAULT '0',
   `password` varchar(191) NOT NULL,
   `email` varchar(191) NOT NULL,
-  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `name` varchar(191) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  `name` varchar(191) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,1,'$2y$10$vTje.ndUFKHyuotY99iYkO.2aHJUgOsy2x0RMXP1UmrTe6CQsKbtm','demo@example.com',0,'Demo User');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`user_id`, `is_admin`, `password`, `email`, `deleted`, `name`) VALUES
+(1, 1, '$2y$10$vTje.ndUFKHyuotY99iYkO.2aHJUgOsy2x0RMXP1UmrTe6CQsKbtm', 'demo@example.com', 0, 'Demo User');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for dumped tables
+--
 
--- Dump completed on 2017-08-02 13:08:47
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `post_tags`
+--
+ALTER TABLE `post_tags`
+  ADD PRIMARY KEY (`post_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indexes for table `tag`
+--
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`tag_id`);
+
+--
+-- Indexes for table `translations`
+--
+ALTER TABLE `translations`
+  ADD PRIMARY KEY (`translation_id`),
+  ADD UNIQUE KEY `language_phrase_controller_action_index` (`language`,`phrase`,`controller`,`action`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `post_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `tag_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `translations`
+--
+ALTER TABLE `translations`
+  MODIFY `translation_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `post_tags`
+--
+ALTER TABLE `post_tags`
+  ADD CONSTRAINT `post_tags_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`);
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
